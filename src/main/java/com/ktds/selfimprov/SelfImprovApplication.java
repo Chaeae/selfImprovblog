ae85c6dff4f3c8dfa493776d59a7772be981ec2f
hyunchae <hyunchae0818@naver.com>
2024-01-31 17:30:04 +0900

    initial

---
+++ src/main/java/com/ktds/selfimprov/SelfImprovApplication.java
@@ -0,0 +1,13 @@
+package com.ktds.selfimprov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfImprovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfImprovApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ktds/selfimprov/SelfImprovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktds.selfimprov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfImprovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
